2c0d0c48bb43de3f3762d3a25fc98f36ce69bac9
Jamison Wilkes <jamisonwilkes@gmail.com>
2022-02-17 10:02:57 -0700

    fully functioning

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,39 +6,133 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
 
-import React from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { FaReact, FaNode, FaDatabase, FaShippingFast } from 'react-icons/fa'
+import { BrowserRouter as Router, Route, Link, useParams, Redirect, Switch } from "react-router-dom"
+export const VocabContext = createContext()
 
-// Example class component
-export class PoweredBy extends React.Component {
-  render(){
-    return (
-      <h2 className="lead text-muted my-3">Powered by {this.props.framework}</h2>
-    )
-  }
+export function App(props){
+  let [vocabs, setVocabs] = useState([
+    { group: "Pronouns", words: [
+        [ 'i', 'ich' ],
+        [ 'you (informal)', 'du'],
+        [ 'you (formal)', 'Sie' ],
+        [ 'he', 'er' ],
+        [ 'she', 'sie' ],
+        [ 'it', 'es' ],
+        [ 'we', 'wir' ],
+        [ 'you (plural', 'ihr'],
+        [ 'they', 'sie' ]
+      ]
+    }, 
+    { group: "Numbers", words: [
+        [ 'zero', 'null' ],
+        [ 'one', 'eins' ],
+        [ 'two', 'zwei' ],
+        [ 'three', 'drei' ],
+        [ 'four', 'vier' ],
+        [ 'five', 'fünf' ],
+        [ 'six', 'sechs' ],
+        [ 'seven', 'sieben' ],
+        [ 'eight', 'acht' ],
+        [ 'nine', 'neun' ],
+        [ 'ten', 'zehn' ]
+      ]
+    }
+  ])
+  let [color, setColor] = useState("steelblue")
+
+  return (
+    <VocabContext.Provider value={{vocabs, setVocabs, color, setColor}}>
+      <Router>
+        <header style={{ borderBottomColor: color, color: color}}>
+          <h1>German Vocabulary Flash Cards</h1>
+        </header>
+        <Switch>
+          <Route exact path="/vocab"><Vocab /></Route>
+          <Route exact path="/vocab/:gid"><Group /></Route>
+          <Route path="/vocab/:gid/word/:wid"><Word /></Route>
+          <Redirect from="" to="/vocab"></Redirect>
+        </Switch>
+        <footer style={{ backgroundColor: color}}>
+          &copy; 2021 WEB 3430 - All rights reserved
+        </footer>
+      </Router>
+    </VocabContext.Provider>
+   )
 }
 
-// Example function components
-export function MERNDisplay(props) {
+export function Vocab(props){
+  let { vocabs, setVocabs, color, setColor } = useContext(VocabContext)
+  
+  useEffect(() => {
+    setColor("var(--purple")
+  },[])
+  
   return (
-    <div className="course-icons">
-      <FaNode size="30%" color="#43853d" className="mt-5"/><br/>
-      <FaReact size="20%" color="skyblue" className="m-3" />
-      <FaShippingFast size="20%" color="#259dff" className="m-3"/>
-      <FaDatabase size="20%" color="#00ED64" className="m-3"/>
+    <div className="vbox">
+      <h1>{vocabs.length} groups</h1>
+      <nav>
+        <ul>
+          {
+            vocabs.map((g, ndx) => {
+              return <li key={ndx}><Link to={"/vocab/" + ndx}>{g.group}</Link></li>
+            })
+          }
+        </ul>
+      </nav>
     </div>
   )
 }
 
-export function Welcome(props) {
+export function Group(props){
+  let { vocabs, setVocabs, color, setColor } = useContext(VocabContext)
+  let {gid} = useParams()
+
+  useEffect(() => {
+    setColor("var(--orange")
+  },[])
   return (
-    <div className="container my-5 py-5 text-center">
-      <MERNDisplay/>
-      <h1 className="mt-3 display-5">Welcome to {props.to}</h1>
-      <PoweredBy framework={`React from ${props.using}`}/>
+    <div className="gbox">
+      <h1>{vocabs[+gid].words.length}</h1>
+      <nav>
+        <ul>
+          <li><Link to="/vocab">Main page</Link></li>
+          <li><Link to={"/vocab/" + gid + "/word/0"}>Start Here</Link></li>
+        </ul>
+      </nav>
     </div>
   )
 }
 
-ReactDOM.render(<Welcome to="WEB 3430" using="the MERN stack"/>, document.getElementById('main'))
+export function Word(props){
+  let { vocabs, setVocabs, color, setColor } = useContext(VocabContext)
+  let {gid, wid} = useParams()
+
+  useEffect(() => {
+    setColor("var(--teal")
+  },[])
+
+  return (
+    <div className="wbox">
+      <h1>{vocabs[+gid].words[+wid][1]}</h1>
+      <h3>{vocabs[+gid].words[+wid][0]}</h3>
+      <nav>
+        <ul>
+          <li><Link to="/vocab">Main page</Link></li>
+          {
+            (+wid) - 1 >= 0
+            ? <li><Link to={"/vocab/" + gid + "/word/" +((+wid) -1) }>&lt;&lt; Prev</Link></li>
+            : <></>
+          }
+          {
+            (+wid) + 1 < vocabs[+gid].words.length
+            ? <li><Link to={"/vocab/" + gid + "/word/" +((+wid) +1) }>Next &gt;&gt; </Link></li>
+            : <></>
+          }
+        </ul>
+      </nav>
+    </div>
+  )
+}
+ReactDOM.render(<App/>, document.getElementById('main'))
